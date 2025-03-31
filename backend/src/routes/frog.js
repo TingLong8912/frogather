0d7e1ffc6386f793ba8ca42edabaa1b47cc2b9c7
TingLong8912 <d1z59jzm3@gmail.com>
2025-03-31 14:24:16 +0800

    remove backedn

--- backend/src/routes/frog.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getFrogs } from "./../controllers/frogs";
-
-const router = Router();
-
-/* ROUTER */
-router.get("/", getFrogs);
-
-export default router;
