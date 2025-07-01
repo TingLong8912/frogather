92371e435d39359a36896768d4a562173a85f27e
TingLong8912 <cljh303@gmail.com>
2025-07-02 02:19:22 +0800

    change api route

--- src/connection.js
+++ src/connection.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const API_PORT = 
-  (process.env.NODE_ENV === "production") ? 
-  "/" 
-  : "https://frogather-backend.onrender.com/"
+  "https://frogather-backend.onrender.com/"
 ;
 
 const instance = axios.create({
